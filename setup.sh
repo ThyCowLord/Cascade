f654e4633cd8e016d0e286ff948022696a8978a9
ThyCowLord <34698219+ThyCowLord@users.noreply.github.com>
2018-03-07 21:09:18 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python-picamera
+sudo apt-get install apache2 -y
+
+cd ~
+git clone https://github.com/ThyCowLord/Cascade
+cd Cascade
+yes | sudo cp index.html ~/var/www/html/index.html
+yes | sudo cp rc.local /etc/rc.local
+sudo update-rc.d apache2 defaults
+
+echo 'Setup complete! Do not delete these files!'
