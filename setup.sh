5963c8e62a1f26b86cc4fef4b22336b693ff447f
ThyCowLord <34698219+ThyCowLord@users.noreply.github.com>
2018-03-07 21:10:46 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ git clone https://github.com/ThyCowLord/Cascade
 cd Cascade
 yes | sudo cp index.html ~/var/www/html/index.html
 yes | sudo cp rc.local /etc/rc.local
+sudo chmod a+x /etc/rc.local
+
 sudo update-rc.d apache2 defaults
 
 echo 'Setup complete! Do not delete these files!'
